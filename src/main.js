b2ab7686a9013a71169a8327a5f4ec14eae74fd1
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-08-21 04:19:54 +0300

    do half

--- src/main.js
+++ src/main.js
@@ -8,23 +8,27 @@ import { fetchByUserKey } from './js/pixabay-api';
 const formEL = document.querySelector('.search-form');
 const galleryEL = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more-btn');
+const loaderEl = document.createElement('p');
+loaderEl.classList.add('loader');
 const galleryModal = new SimpleLightbox('.gallery a');
 loadMoreBtn.style.display = 'none';
 
 let page = 1;
+let userKeywordForLoadMore;
+let totalPages;
 
 const onFormElSubmit = async event => {
-	event.preventDefault();
+	event.preventDefault();	
 	const userKeyword = event.target.elements.user_keyword.value;
 	if (userKeyword.trim() === '') {
 		return;
 	}
+	userKeywordForLoadMore = userKeyword;
 	galleryEL.innerHTML = '';
+	loadMoreBtn.style.display = 'none';
 	page = 1;
 	
-	const loaderEl = document.createElement('p');
-	loaderEl.classList.add('loader');
-	formEL.after(loaderEl);
+	galleryEL.after(loaderEl);
 	
 	try {
 		const response = await fetchByUserKey(userKeyword, page);
@@ -39,26 +43,41 @@ const onFormElSubmit = async event => {
 			galleryEL.innerHTML = createMarkup(response.data.hits);
 			galleryModal.refresh();
 			loaderEl.remove();
-		formEL.reset();	
-		loadMoreBtn.style.display = 'block';
-		const onLoadMoreClick = async () => {
-			try {
-				page += 1;
-				const response = await fetchByUserKey(userKeyword, page);
-				const markup = createMarkup(response.data.hits);
-				galleryEL.insertAdjacentHTML('beforeend', markup);
-				galleryModal.refresh();
-			 } catch (error) {
-		 console.log(error);
-  		}
-		}
-		loadMoreBtn.addEventListener('click' , onLoadMoreClick)
+			formEL.reset();	
+			page += 1;
+			totalPages = Math.ceil(response.data.totalHits / 15)
+    		if (totalPages > 1) {
+				 loadMoreBtn.style.display = 'block';					 
+			}
 		} catch (error) {
-		console.log(error);
-  		}
-	
+			console.log(error);
+  		}	
 }
 
-formEL.addEventListener('submit', onFormElSubmit);
+const onLoadMoreClick = async () => {
+	galleryEL.after(loaderEl);
+	loadMoreBtn.style.display = 'none';
+			try {
+				const response = await fetchByUserKey(userKeywordForLoadMore, page);
+				const markup = createMarkup(response.data.hits);
+				loaderEl.remove();
+				galleryEL.insertAdjacentHTML('beforeend', markup);
+				galleryModal.refresh();
+         	loadMoreBtn.style.display = 'block';
+				page += 1;
+		   	 } catch (error) {
+				 console.log(error);
+				}
+				if (page > totalPages) {
+					iziToast.info({
+						message: "We're sorry, but you've reached the end of search results.",
+						position: 'topRight',
+					});
+					loadMoreBtn.style.display = 'none';
+					return; 
+					} 
+		}
 
+formEL.addEventListener('submit', onFormElSubmit);
+loadMoreBtn.addEventListener('click', onLoadMoreClick);
 
